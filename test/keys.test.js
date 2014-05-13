3dac9482a292ecc80a9f7397b0ed1390e52e0bd4
Chad Engler <englercj@live.com>
2014-05-13 09:02:18 -0700

    initial rewrite to be obj <-> buffer serializer

--- test/keys.test.js
+++ test/keys.test.js
@@ -1,29 +1,44 @@
-
 require('should');
-var buffer = require('buffer');
 
 var bufferpack = require('..');
 
 describe('Keying', function() {
-  var values = [1, 2, 3, 'atest', 'somethingelse'];
-  var format = '<b(first)b(second)b(third)5s(other)S(something)';
+    var values = {
+            first: 1,
+            second: 2,
+            third: 3,
+            other: 'atest',
+            something: 'somethingelse',
+            deep: {
+                num: 10,
+                flt: 1.567,
+                ch: 'h'
+            }
+        },
+        format = '<b(first)b(second)b(third)5s(other)S(something){i(num)f(flt)c(ch)}(deep)',
+        packed = bufferpack.pack(format, values);
 
-  var packed = bufferpack.pack(format, values);
+    describe('#unpack()', function() {
+        var unpacked = bufferpack.unpack(format, packed);
 
-  describe('#unpack()', function() {
-    var unpacked = bufferpack.unpack(format, packed, 0);
+        it('should return an object', function() {
+            unpacked.should.be.type('object');
+            unpacked.should.have.property('first', values.first);
+            unpacked.should.have.property('second', values.second);
+            unpacked.should.have.property('third', values.third);
+            unpacked.should.have.property('other', values.other);
+            unpacked.should.have.property('something', values.something);
 
-    it('should return an object', function() {
-      unpacked.should.be.type('object');
-      unpacked.should.have.property('first', 1);
-      unpacked.should.have.property('second', 2);
-      unpacked.should.have.property('third', 3);
-      unpacked.should.have.property('other', 'atest');
-      unpacked.should.have.property('something', 'somethingelse');
+            unpacked.should.have.property('deep');
+            unpacked.deep.should.be.type('object');
+            unpacked.deep.should.have.property('num', values.deep.num);
+            unpacked.deep.should.have.property('flt');
+            unpacked.deep.flt.should.be.approximately(values.deep.flt, 0.0001);
+            unpacked.deep.should.have.property('ch', values.deep.ch);
+        });
+
+        it('should returned object should have ' + Object.keys(values).length + ' properties', function() {
+            Object.keys(unpacked).length.should.equal(Object.keys(values).length);
+        });
     });
-
-    it('should returned object should have 5 properties', function() {
-      Object.keys(unpacked).length.should.equal(5);
-    });
-  });  
 });
